f5883c9777152b83e1e5b80762a6ddc24b8369bc
Renan <104283869+Renan023@users.noreply.github.com>
2024-02-26 23:14:36 -0300

    Add files via upload

---
+++ jogo/jogo-da-memoria/src/engine/engine.js
@@ -0,0 +1,50 @@
+const emojis = [
+    "😊",
+    "😊",
+    "😘",
+    "😘",
+    "😶‍🌫️",
+    "😶‍🌫️",
+    "😓",
+    "😓",
+    "🙊",
+    "🙊",
+    "🐺",
+    "🐺"
+]
+
+let openCards = []
+
+let shuffleEmojis = emojis.sort(()=>(Math.random() > 0.5 ? 1: -1))
+
+for(let i = 0; i< emojis.length;i++){
+    let box = document.createElement('div');
+    box.className = "item"
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box)
+}
+
+function handleClick(){
+    if(openCards.length<2){
+        this.classList.add('boxOpen');
+        openCards.push(this)
+    }
+    if(openCards.length==2){
+        setTimeout(check,500)
+    }
+}
+
+function check(){
+    if(openCards[0].innerHTML=== openCards[1].innerHTML){
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+        }else{
+            openCards[0].classList.remove("boxOpen");
+            openCards[1].classList.remove("boxOpen");
+        }
+        openCards = [];
+        if(document.querySelectorAll("bnoxMatch").length=== emojis.length){
+            alert("Você venceu!")
+        }
+}
